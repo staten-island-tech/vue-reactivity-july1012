eaa41c1a0f511c119c7d11140aa2719cb3b4a011
july1012 <113192987+july1012@users.noreply.github.com>
2024-02-26 13:15:42 -0500

    fd

--- vite-project/src/views/GardenView.vue
+++ vite-project/src/views/GardenView.vue
@@ -2,19 +2,15 @@
   <div>
     <FlowerCard v-for="flower in flowers" 
     :key="flower.name" 
-    :Flower="flower"/>
+    :Flower="flower" />
 
     <h2>This is your bouquet</h2>
 
     <PickedCard 
     v-for="flower in flowers"
     :key="flower.name"
-    :Picked="flower"
+    :Picked="flower" />
 
-    />
-
-
-   
   </div>
 </template>
 
@@ -22,40 +18,45 @@
 import FlowerCard from '@/components/FlowerCard.vue';
 import PickedCard from '@/components/PickedCard.vue';
 
-
 const flowers = [
         {
-          name: "Venetian Renaissance",
+          name: "✧༺♥༻∞", //tulip
          picked: false,
           price: 1300,
           img: "https://images.unsplash.com/photo-1498307833015-e7b400441eb8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80&quot;",
         },
         {
-          name: "Swiss Mountain Getaway",
+          name: "𓆩♡𓆪",
             picked: false,
           price: 800,
           img: "https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1476&q=80&quot;",
         },
         {
-          name: "Ibizan Coastal Vacation",
+          name: "*:･ﾟ✧*:･ﾟ",
             picked: false,
           price: 1200,
           img: "https://images.unsplash.com/photo-1547138494-97041dec734b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80&quot;",
         },
         {
-          name: "Hawaiian Vacation",
+          name: "'*•.¸♡ ♡¸.•*'",
             picked: false,
           price: 900,
           img: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80&quot;",
         },
         {
-          name: "Cascade Mountains",
+          name: "˚୨୧⋆｡˚ ⋆",
             picked: false,
           price: 400,
           img: "https://images.unsplash.com/photo-1511497584788-876760111969?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1332&q=80&quot;",
         },
         {
-          name: "Egyptian Exploration",
+          name: "₍ ᐢ.ˬ.ᐢ₎˚୨୧",
+            picked: false,
+          price: 800,
+          img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+        },
+        {
+          name: "‧̍̊˙˚˙ᵕ꒳ᵕ˙˚˙",
             picked: false,
           price: 800,
           img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
