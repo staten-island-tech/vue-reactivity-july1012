54e22e4081e6bb77c14255277559221713600250
july1012 <113192987+july1012@users.noreply.github.com>
2024-02-25 20:42:58 -0500

    hello?

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -9,3 +9,14 @@
 //       document.body.classList.remove("pinkk");
 //     }
 //   });
+
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue ({
+    router,
+    render: h=> h(App)
+}).$mount('#app')
